e5275149f1f7321b8cb882e27f43b0e7c78367f9
user <abdukarimov9900@gmail.com>
2023-01-29 22:51:01 +0500

    task 3 UI latest self

--- lib/pages/task3/task3_page.dart
+++ lib/pages/task3/task3_page.dart
@@ -42,113 +42,123 @@ class _TaskThreePageState extends State<TaskThreePage> {
           create: (BuildContext context) => Task3Cubit()..getCards(),
           child:
               BlocBuilder<Task3Cubit, Task3State>(builder: (cubitCTX, state) {
-            if (state is Task3Loading) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            } else if (state is Task3Loaded) {
-              return Container(
-                  height: 812.h,
-                  child: ListView.builder(
-                    itemCount: 1,
-                    itemBuilder: (ctx, i) => Container(
-                      height: 250,
-                      width: double.infinity,
-                      margin:
-                          EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(30),
-                        ),
-                        color: Colors.indigo,
-                      ),
-                      child: Column(
-                        children: [
-                          Container(
-                            alignment: Alignment.topLeft,
-                            margin: EdgeInsets.only(left: 15, top: 15),
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              '1111 1111 1111 1111',
-                              style: TextStyle(
-                                  fontSize: 22,
-                                  fontWeight: FontWeight.w700,
-                                  color: Colors.white),
-                            ),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Container(
-                                margin: EdgeInsets.only(left: 15),
-                                alignment: Alignment.topLeft,
-                                padding: const EdgeInsets.all(8.0),
-                                child: Text(
-                                  '11/23',
-                                  style: TextStyle(
-                                      fontSize: 22,
-                                      fontWeight: FontWeight.w700,
-                                      color: Colors.white),
+                if (state is Task3Loading) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (state is Task3Loaded) {
+                  return Container(
+                      height: 812.h,
+                      child: ListView.builder(
+                        itemCount: state.cards.length,
+                        itemBuilder: (ctx, i) =>
+                            Container(
+                              height: 250,
+                              width: double.infinity,
+                              margin:
+                              EdgeInsets.symmetric(
+                                  horizontal: 15, vertical: 15),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(30),
                                 ),
+                                color: Colors.indigo,
                               ),
-                              Container(
-                                margin: EdgeInsets.only(right: 15),
-                                height: 30,
-                                width: 60,
-                                child: Image.asset(
-                                  'assets/uzcard.png',
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                            ],
-                          ),
-                          Container(
-                              margin: EdgeInsets.only(left: 15),
-                              alignment: Alignment.topLeft,
-                              padding: const EdgeInsets.all(8.0),
-                              child: Text(
-                                'Karta Nomi',
-                                style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.w700,
-                                    color: Colors.white),
-                              )),
-                          Container(
-                            margin: EdgeInsets.all(15),
-                            padding: const EdgeInsets.all(8.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  'Palonchiev Pismadonchi',
-                                  style: TextStyle(
-                                      fontSize: 22,
-                                      fontWeight: FontWeight.w700,
-                                      color: Colors.white),
-                                ),
-                                Row(
-                                  children: [
-                                    IconButton(
-                                      onPressed: () {},
-                                      icon: Icon(Icons.edit),
-                                      color: Colors.white,
+                              child: Column(
+                                children: [
+                                  Container(
+                                    alignment: Alignment.topLeft,
+                                    margin: EdgeInsets.only(left: 15, top: 15),
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text(
+                                      state.cards[i].cardNumber,
+                                      style: TextStyle(
+                                          fontSize: 22,
+                                          fontWeight: FontWeight.w700,
+                                          color: Colors.white),
                                     ),
-                                    IconButton(
-                                      onPressed: () {},
-                                      icon: Icon(Icons.delete),
-                                      color: Colors.white,
-                                    )
-                                  ],
-                                )
-                              ],
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment
+                                        .spaceBetween,
+                                    children: [
+                                      Container(
+                                        margin: EdgeInsets.only(left: 15),
+                                        alignment: Alignment.topLeft,
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Text(
+                                          state.cards[i].expireDate,
+                                          style: TextStyle(
+                                              fontSize: 22,
+                                              fontWeight: FontWeight.w700,
+                                              color: Colors.white),
+                                        ),
+                                      ),
+                                      Container(
+                                        margin: EdgeInsets.only(right: 15),
+                                        height: 30,
+                                        width: 60,
+                                        child: Image.asset(
+                                          'assets/${state.cards[i].type
+                                              ? "uzcard.png"
+                                              : "Humo-01.jpg" }',
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  Container(
+                                      margin: EdgeInsets.only(left: 15),
+                                      alignment: Alignment.topLeft,
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                        'Karta Nomi',
+                                        style: TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.w700,
+                                            color: Colors.white),
+                                      )),
+                                  Container(
+                                    margin: EdgeInsets.all(15),
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment
+                                          .spaceBetween,
+                                      children: [
+                                        Text(
+                                          state.cards[i].owner,
+                                          style: TextStyle(
+                                              fontSize: 22,
+                                              fontWeight: FontWeight.w700,
+                                              color: Colors.white),
+                                        ),
+                                        Row(
+                                          children: [
+                                            IconButton(
+                                              onPressed: () {},
+                                              icon: Icon(Icons.edit),
+                                              color: Colors.white,
+                                            ),
+                                            IconButton(
+                                              onPressed: () {},
+                                              icon: Icon(Icons.delete),
+                                              color: Colors.white,
+                                            )
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ));
-            }
-          }),
+                      ));
+                }
+
+                else {
+                  return Center(child : Text("else"));
+              }
+              }),
         ));
   }
 }
